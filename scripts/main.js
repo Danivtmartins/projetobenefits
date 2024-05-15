8090e4046c314ce1f66166a4d42f6de59dcf5cf9
unknown <danielatavares.contato@gmail.com>
2024-05-15 10:11:17 +0100

    close modal event added

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,24 @@
 if (typeof document !== 'undefined') {
   const modal = document.querySelector(".modal"); //querySelector() returns the first Element within the document that matches the specified selector, or group of 
   const openModalBtn = document.querySelector(".btn-open");
+  const closeModalBtn = document.querySelector(".close");
 
 
-
+//open modal function
   function openModal() {
-    modal.classList.remove("hidden");
+    modal.classList.remove("hidden")
   };
-
+//open modal event
 openModalBtn.addEventListener("click", openModal);
+
+
+//close modal function
+const closeModal = function () {
+  modal.classList.add("hidden")
+};
+
+//close modal event
+closeModalBtn.addEventListener("click", closeModal);
+
 
 }
